12d4493eede6866c4fc5192e5f00f92ec9b65017
Joseph Calcagno <j_calca2@uncg.edu>
2025-02-06 11:25:10 -0500

    Put Post and Delete fundamentals

--- springboot-rest-api/src/main/java/Joe/coursework/springboot_rest_api/controller/StudentController.java
+++ springboot-rest-api/src/main/java/Joe/coursework/springboot_rest_api/controller/StudentController.java
@@ -3,9 +3,15 @@ package Joe.coursework.springboot_rest_api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import Joe.coursework.springboot_rest_api.bean.Student;
@@ -56,4 +62,32 @@ public class StudentController {
 
     }
 
+    //Spring boot rest api that handles HTTP Post Request - create resource
+    //@PostMapping and @RequestBody
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        
+        return student;
+    }
+
+    // Spring boot REST API that handles http put Reuqest - updating resource
+    // http://localhost:8080/students/3/update
+    @PutMapping("students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    // Spring Boot REST API that handles HTTP delete Request - deleting resource
+    @DeleteMapping("students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return "Student deleted successfully";
+    }
+    
 }
